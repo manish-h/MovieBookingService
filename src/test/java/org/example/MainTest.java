6e53c8a93a173b87224a4e78d95560da1f389994
manishh <hotwani.manish@gmail.com>
2025-09-14 09:21:45 +0530

    Initial commit

---
+++ src/test/java/org/example/MainTest.java
@@ -0,0 +1,30 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MainTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    public void helloShouldReturnDefaultMessage() {
+        assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+                String.class)).isEqualTo("Hello, World!");
+    }
+
+}
